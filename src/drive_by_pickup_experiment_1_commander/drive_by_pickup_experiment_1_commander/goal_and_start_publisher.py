86cb99ec5e9e54a8375ca54cce33dea4a8617ba1
Alfred Thoft <alfchr21@student.aau.dk>
2024-04-14 11:47:42 +0200

    dependency fixes

--- src/drive_by_pickup_experiment_1_commander/drive_by_pickup_experiment_1_commander/goal_and_start_publisher.py
+++ src/drive_by_pickup_experiment_1_commander/drive_by_pickup_experiment_1_commander/goal_and_start_publisher.py
@@ -1,12 +1,27 @@
 import rclpy
+from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator
+from sensor_msgs.msg import LaserScan
 
+
+class DistanceToTable(Node):
+    def __init__(self):
+        super().__init__('distance_to_table')
+        self._scan_sub = self.create_subscription(msg_type=LaserScan, 
+                                                  topic='b_scan',
+                                                  callback=self.scan_callback,
+                                                  qos_profile=10)
+        self.get_logger().info("distance to table node created")
+    
+    def scan_callback(self, msg):
+        self.get_logger().info(f"got scan data {len(msg.ranges)}")
 
 def main():
     rclpy.init()
     navigator = BasicNavigator()
-    
+    scan_sub = DistanceToTable()
+    rclpy.spin_once(scan_sub)
     #setting the initial pose for the experiment 
     initial_pose = PoseStamped()
     initial_pose.header.stamp = navigator.get_clock().now().to_msg()
